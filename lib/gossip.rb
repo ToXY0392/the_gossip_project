5cb3485bc4027980ed2928ef8e4894d42b7cef15
toxy0392 <vinou0392@gmail.com>
2026-02-06 14:30:01 +0100

    gossip changes

--- lib/gossip.rb
+++ lib/gossip.rb
@@ -55,3 +55,29 @@ class Gossip
     return true
   end
 end
+class Gossip
+  attr_reader :author, :content
+
+  def initialize(author, content)
+    @content = content
+    @author = author
+  end
+
+  def save
+    CSV.open("db/gossip.csv", "ab") do |csv|
+      csv << [author, content]
+    end
+  end
+
+  def self.all
+    all_gossips = []
+    CSV.read("db/gossip.csv").each do |row|
+      all_gossips << Gossip.new(row[0], row[1])
+    end
+    all_gossips
+  end
+
+  def self.delete(index)
+    # …
+  end
+end
